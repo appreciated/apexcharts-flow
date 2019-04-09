f20dacc5a2dc8be5dc2b706aab6ca8fc4ac80788
Johannes Goebel <GoebelJohannes@gmx.net>
2019-04-09 12:14:43 +0200

    moved data to client side

--- src/main/java/com/github/appreciated/apexcharts/ApexCharts.java
+++ src/main/java/com/github/appreciated/apexcharts/ApexCharts.java
@@ -14,146 +14,213 @@ import java.util.Map;
 @HtmlImport("frontend://com/github/appreciated/apexcharts/apexcharts-wrapper.html")
 public class ApexCharts extends PolymerTemplate<ApexCharts.ApexChartModel> implements HasSize {
 
-    ApexAnnotations annotations;
-    ApexChart chart;
-    String[] colors;
-    ApexDataLabels dataLabels;
-    ApexFill fill;
-    ApexGrid grid;
-    String[] labels;
-    ApexLegend legend;
-    ApexMarkers markers;
-    ApexNoData noData;
-    ApexPlotOptions plotOptions;
-    ApexResponsive[] responsive;
-    ApexAxisChartSeries series; //?:  | ApexNonAxisChartSeries;
-    ApexStates states;
-    ApexStroke stroke;
-    ApexTitleSubtitle subtitle;
-    ApexTheme theme;
-    ApexTitleSubtitle title;
-    ApexTooltip tooltip;
-    ApexXAxis xaxis;
-    ApexYAxis[] yaxis;
-    private double[] doubleSeries;
-    private Map<String, Double> mapSeries;
-
     public ApexCharts() {
         setWidth("300px");
         setHeight("300px");
     }
 
     public ApexCharts withAnnotations(ApexAnnotations annotations) {
-        this.annotations = annotations;
+        getModel().setAnnotations(annotations);
         return this;
     }
 
     public ApexCharts withChart(ApexChart chart) {
-        this.chart = chart;
+        getModel().setChart(chart);
         return this;
     }
 
     public ApexCharts withColors(String[] colors) {
-        this.colors = colors;
+        getModel().setColors(colors);
         return this;
     }
 
     public ApexCharts withDataLabels(ApexDataLabels dataLabels) {
-        this.dataLabels = dataLabels;
+        getModel().setDataLabels(dataLabels);
         return this;
     }
 
     public ApexCharts withFill(ApexFill fill) {
-        this.fill = fill;
+        getModel().setFill(fill);
         return this;
     }
 
     public ApexCharts withGrid(ApexGrid grid) {
-        this.grid = grid;
+        getModel().setGrid(grid);
         return this;
     }
 
-    public ApexCharts withLabels(String ... labels) {
-        this.labels = labels;
+    public ApexCharts withLabels(String... labels) {
+        getModel().setLabels(labels);
         return this;
     }
 
     public ApexCharts withLegend(ApexLegend legend) {
-        this.legend = legend;
+        getModel().setLegend(legend);
         return this;
     }
 
     public ApexCharts withMarkers(ApexMarkers markers) {
-        this.markers = markers;
+        getModel().setMarkers(markers);
         return this;
     }
 
     public ApexCharts withNoData(ApexNoData noData) {
-        this.noData = noData;
+        getModel().setNoData(noData);
         return this;
     }
 
     public ApexCharts withPlotOptions(ApexPlotOptions plotOptions) {
-        this.plotOptions = plotOptions;
+        getModel().setPlotOptions(plotOptions);
         return this;
     }
 
-    public ApexCharts withResponsive(ApexResponsive ... responsive) {
-        this.responsive = responsive;
+    public ApexCharts withResponsive(ApexResponsive... responsive) {
+        getModel().setResponsive(responsive);
         return this;
     }
 
-    public ApexCharts withSeries(double ... series) {
-        this.doubleSeries = series;
+    public ApexCharts withSeries(double... series) {
+        getModel().setDoubleChartSeries(series);
         return this;
     }
-    public ApexCharts withSeries(Map<String,Double> series) {
-        this.mapSeries = series;
+
+    public ApexCharts withSeries(Map<String, Double> series) {
+        getModel().setMapChartSeries(series);
         return this;
     }
 
     public ApexCharts withStates(ApexStates states) {
-        this.states = states;
+        getModel().setStates(states);
         return this;
     }
 
     public ApexCharts withStroke(ApexStroke stroke) {
-        this.stroke = stroke;
+        getModel().setStroke(stroke);
         return this;
     }
 
     public ApexCharts withSubtitle(ApexTitleSubtitle subtitle) {
-        this.subtitle = subtitle;
+        getModel().setSubtitle(subtitle);
         return this;
     }
 
     public ApexCharts withTheme(ApexTheme theme) {
-        this.theme = theme;
+        getModel().setTheme(theme);
         return this;
     }
 
     public ApexCharts withTitle(ApexTitleSubtitle title) {
-        this.title = title;
+        getModel().setTitle(title);
         return this;
     }
 
     public ApexCharts withTooltip(ApexTooltip tooltip) {
-        this.tooltip = tooltip;
+        getModel().setTooltip(tooltip);
         return this;
     }
 
     public ApexCharts withXaxis(ApexXAxis xaxis) {
-        this.xaxis = xaxis;
+        getModel().setXaxis(xaxis);
         return this;
     }
 
     public ApexCharts withYaxis(ApexYAxis[] yaxis) {
-        this.yaxis = yaxis;
+        getModel().setYaxis(yaxis);
         return this;
     }
 
     public interface ApexChartModel extends TemplateModel {
+        ApexAnnotations getAnnotations();
 
+        void setAnnotations(ApexAnnotations annotations);
+
+        ApexChart getChart();
+
+        void setChart(ApexChart chart);
+
+        String[] getColors();
+
+        void setColors(String[] colors);
+
+        ApexDataLabels getDataLabels();
+
+        void setDataLabels(ApexDataLabels dataLabels);
+
+        ApexFill getFill();
+
+        void setFill(ApexFill fill);
+
+        ApexGrid getGrid();
+
+        void setGrid(ApexGrid grid);
+
+        String[] getLabels();
+
+        void setLabels(String[] labels);
+
+        ApexLegend getLegend();
+
+        void setLegend(ApexLegend legend);
+
+        ApexMarkers getMarkers();
+
+        void setMarkers(ApexMarkers markers);
+
+        ApexNoData getNoData();
+
+        void setNoData(ApexNoData noData);
+
+        ApexPlotOptions getPlotOptions();
+
+        void setPlotOptions(ApexPlotOptions plotOptions);
+
+        ApexResponsive[] getResponsive();
+
+        void setResponsive(ApexResponsive[] responsive);
+
+        ApexAxisChartSeries getSeries();
+
+        void setSeries(ApexAxisChartSeries series);
+
+        ApexStates getStates();
+
+        void setStates(ApexStates states);
+
+        ApexStroke getStroke();
+
+        void setStroke(ApexStroke stroke);
+
+        ApexTitleSubtitle getSubtitle();
+
+        void setSubtitle(ApexTitleSubtitle subtitle);
+
+        ApexTheme getTheme();
+
+        void setTheme(ApexTheme theme);
+
+        ApexTitleSubtitle getTitle();
+
+        void setTitle(ApexTitleSubtitle title);
+
+        ApexTooltip getTooltip();
+
+        void setTooltip(ApexTooltip tooltip);
+
+        ApexXAxis getXaxis();
+
+        void setXaxis(ApexXAxis xaxis);
+
+        ApexYAxis[] getYaxis();
+
+        void setYaxis(ApexYAxis[] yaxis);
+
+        double[] getDoubleChartSeries();
+
+        void setDoubleChartSeries(double[] doubleChartSeries);
+
+        Map<String, Double> getMapChartSeries();
+
+        void setMapChartSeries(Map<String, Double> mapChartSeries);
     }
 }
 
