45f100c8bcb30ab2927bb52af24bb3ddf458dc14
Johannes Goebel <GoebelJohannes@gmx.net>
2019-04-23 11:19:18 +0200

    - removed unused classes - updated apexcharts.js from 3.6.6 to 3.6.8

--- src/main/java/com/github/appreciated/apexcharts/encoders/YAxisEncoder.java
+++
@@ -1,7 +0,0 @@
-package com.github.appreciated.apexcharts.encoders;
-
-import com.github.appreciated.apexcharts.JsonEncoder;
-import com.github.appreciated.apexcharts.config.YAxis;
-
-public class YAxisEncoder extends JsonEncoder<YAxis> {
-}
