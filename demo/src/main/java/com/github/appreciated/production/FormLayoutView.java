aefc66ccf98e6eed9f1ac2915d76c0d958fd1656
Johannes Goebel <GoebelJohannes@gmx.net>
2019-04-27 16:10:58 +0200

    - refactored project structure - added demo with production mode

---
+++ demo/src/main/java/com/github/appreciated/production/FormLayoutView.java
@@ -0,0 +1,25 @@
+package com.github.appreciated.production;
+
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "form-layout")
+public class FormLayoutView extends FormLayout {
+    public FormLayoutView() {
+        ExampleChartGenerator generator = new ExampleChartGenerator();
+        add(generator.getPieChart());
+        add(generator.getDonutChart());
+        add(generator.getLineChart());
+        add(generator.getAreaChart());
+        add(generator.getBubbleChart());
+        add(generator.getHorizontalBarChart());
+        add(generator.getVerticalBarChart());
+        add(generator.getRadialBarChart());
+        add(generator.getGradientRadialBarChart());
+        add(generator.getMultiRadialBarChart());
+        add(generator.getCandleStickChart());
+        add(generator.getRadarChart());
+        add(generator.getScatterChart());
+        add(generator.getHeatMapChart());
+    }
+}
