a55e14c929162909f69f0d31a77f615224ebedde
appreciated <goebeljohannes@gmx.net>
2019-08-11 22:39:45 +0200

    - removed comments

--- src/test/java/com/github/appreciated/ExampleChartGenerator.java
+++ src/test/java/com/github/appreciated/ExampleChartGenerator.java
@@ -54,50 +54,6 @@ public class ExampleChartGenerator {
         };
     }
 
-    /**
-     * var options = {
-     * <p>
-     * series: [{
-     * name: '',
-     * data: [{
-     *
-     * <p>
-     * }, {
-     * <p>
-     * }, {
-     * name: '',
-     * data: [{
-     * x: 'Design',
-     * y: [new Date('2019-03-01').getTime(), new Date('2019-03-02').getTime()]
-     * }, {
-     * x: 'Code',
-     * y: [new Date('2019-03-03').getTime(), new Date('2019-03-07').getTime()]
-     * }, {
-     * x: 'Test',
-     * y: [new Date('2019-03-06').getTime(), new Date('2019-03-09').getTime()]
-     * }, {
-     * x: 'Deployment',
-     * y: [new Date('2019-03-10').getTime(), new Date('2019-03-11').getTime()]
-     * }]
-     * }],
-     * <p>
-     * fill: {
-     * type: 'gradient',
-     * gradient: {
-     * shade: 'light',
-     * type: "vertical",
-     * shadeIntensity: 0.25,
-     * gradientToColors: undefined,
-     * inverseColors: true,
-     * opacityFrom: 1,
-     * opacityTo: 1,
-     * stops: [50, 0, 100, 100]
-     * }
-     * }
-     * }
-     *
-     * @return
-     */
     private ApexCharts getTimelineChart() {
         ApexCharts timelineChart = new ApexCharts()
                 .withChart(ChartBuilder.get()
