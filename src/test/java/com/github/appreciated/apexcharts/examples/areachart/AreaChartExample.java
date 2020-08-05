183b189b6844863a0f773dbc6413f997c6697b00
Detlef Boehm <detlef.boehm@christmann.info>
2020-08-05 19:42:02 +0200

    Minor debugging changes to  AreaChartExample

--- src/test/java/com/github/appreciated/apexcharts/examples/areachart/AreaChartExample.java
+++ src/test/java/com/github/appreciated/apexcharts/examples/areachart/AreaChartExample.java
@@ -34,13 +34,13 @@ private static Logger mLog = LoggerFactory.getLogger("myLogger");
                 .withSubtitle(TitleSubtitleBuilder.get()
                         .withText("Price Movements")
                         .withAlign(Align.left).build())
-                .withLabels(IntStream.range(1, 10).boxed().map(day -> LocalDateTime.of(2000, 1, 1, day,0, 0).toString()).toArray(String[]::new))
+                .withLabels(IntStream.range(1, 10).boxed().map(minute -> LocalDateTime.of(2020, 6, 26, 12, minute, 0).toString()).toArray(String[]::new))
                 .withXaxis(XAxisBuilder.get()
                         .withType(XAxisType.datetime).build())
                 .withYaxis(YAxisBuilder.get()
                         .withOpposite(true).build())
                 .withLegend(LegendBuilder.get().withHorizontalAlign(HorizontalAlign.left).build());
-                mLog.info("{}", LocalDateTime.of(2000, 1, 1, 1,0, 0).toString());
-         System.out.println("LocalDate  " + LocalDateTime.of(2000, 1, 1, 1,0, 0).toString());
+                mLog.info("{}", LocalDateTime.of(2000, 1, 1, 12,0, 0).toString());
+         System.out.println("LocalDate  " + LocalDateTime.of(2020, 6, 26, 12,0, 0).toString());
     }
 }
