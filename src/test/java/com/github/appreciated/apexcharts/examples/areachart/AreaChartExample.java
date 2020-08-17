2eb258b6f66e8c98902ec241bb66a40e1767b7ee
Detlef Boehm <detlef.boehm@christmann.info>
2020-08-17 08:24:07 +0200

    Preparation for pull-request: remove local changes

--- src/test/java/com/github/appreciated/apexcharts/examples/areachart/AreaChartExample.java
+++ src/test/java/com/github/appreciated/apexcharts/examples/areachart/AreaChartExample.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.util.stream.IntStream;
 
 public class AreaChartExample extends ApexChartsBuilder {
+
     public AreaChartExample() {
         withChart(
                 ChartBuilder.get()
