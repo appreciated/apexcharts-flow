a84b0aaae4b78006221102f1c07f870370e9e6a1
4n0nD3v3l0p3r <56003470+4n0nD3v3l0p3r@users.noreply.github.com>
2020-08-18 11:51:16 +0100

    Enable Custom Colour Usage In Time Line (rangeBar) chart (#67)
    * Added Synchronized Charts -
    https://apexcharts.com/docs/chart-types/synchroniz
    
    * Added Synchronized Charts Examples
    https://apexcharts.com/javascript-chart-demos/line-charts/syncing-charts/
    
    * Added Syncronised Charts - reformatted code
    
    * Revert "Added Syncronised Charts - reformatted code"
    
    This reverts commit 7683346fc179852853d40aef04456c15264f433c.
    
    * Revert "Added Synchronized Charts -"
    
    This reverts commit d1a15cfe27c453fb64249533704c8f4c2e8e3c0f.
    
    * Update Chart.java
    
    Group field added
    
    * Update ChartBuilder.java
    
    added group for sync charts
    
    * Update ExampleChartGenerator.java
    
    added synchronised charts
    
    * Update ExampleChartGenerator.java
    
    * Update SyncronisedLineChartExample1.java
    
    formatted
    
    * Update SyncronisedLineChartExample2.java
    
    formatted
    
    * Added Custom Colour's for rangeBar
    
    https://apexcharts.com/javascript-chart-demos/timeline-charts/distributed/
    
    Co-authored-by: Daniel Hood <Daniel.Hood@sky.uk>

---
+++ src/test/java/com/github/appreciated/apexcharts/examples/timeline/TimeLineChartCustomColorExample.java
@@ -0,0 +1,51 @@
+package com.github.appreciated.apexcharts.examples.timeline;
+
+import com.github.appreciated.apexcharts.ApexChartsBuilder;
+import com.github.appreciated.apexcharts.config.builder.*;
+import com.github.appreciated.apexcharts.config.chart.Type;
+import com.github.appreciated.apexcharts.config.plotoptions.builder.BarBuilder;
+import com.github.appreciated.apexcharts.config.xaxis.XAxisType;
+import com.github.appreciated.apexcharts.helper.ColorCoordinate;
+import com.github.appreciated.apexcharts.helper.ColorDateCoordinate;
+import com.github.appreciated.apexcharts.helper.Coordinate;
+import com.github.appreciated.apexcharts.helper.DateCoordinate;
+import com.github.appreciated.apexcharts.helper.Series;
+
+import java.time.LocalDate;
+
+public class TimeLineChartCustomColorExample extends ApexChartsBuilder {
+    public TimeLineChartCustomColorExample() {
+        withChart(ChartBuilder.get()
+                .withType(Type.rangeBar)
+                .build())
+                .withPlotOptions(PlotOptionsBuilder.get()
+                        .withBar(BarBuilder.get()
+                                .withHorizontal(true)
+                                .withDistributed(true)
+                                .build())
+                        .build())
+                .withDataLabels(DataLabelsBuilder.get()
+                        .withEnabled(false)
+                        .build())
+                .withSeries(new Series<ColorDateCoordinate>("Bob",
+                                new ColorDateCoordinate<>("#008FFB","Design", LocalDate.of(2019, 3, 3), LocalDate.of(2019, 3, 4)),
+                                new ColorDateCoordinate<>("#00E396","Code", LocalDate.of(2019, 3, 3), LocalDate.of(2019, 3, 4)),
+                                new ColorDateCoordinate<>("#775DD0","Test", LocalDate.of(2019, 3, 4), LocalDate.of(2019, 3, 7)),
+                                new ColorDateCoordinate<>("#FEB019","Deployment", LocalDate.of(2019, 3, 11), LocalDate.of(2019, 3, 12))
+                        ),
+                        new Series<ColorDateCoordinate>("Joe",
+                                new ColorDateCoordinate<>("#008FFB","Design", LocalDate.of(2019, 3, 1), LocalDate.of(2019, 3, 2)),
+                                new ColorDateCoordinate<>("#00E396","Code", LocalDate.of(2019, 3, 3), LocalDate.of(2019, 3, 7)),
+                                new ColorDateCoordinate<>("#775DD0","Test", LocalDate.of(2019, 3, 6), LocalDate.of(2019, 3, 9)),
+                                new ColorDateCoordinate<>("#FEB019","Deployment", LocalDate.of(2019, 3, 10), LocalDate.of(2019, 3, 11))
+                        )
+                )
+                .withYaxis(YAxisBuilder.get()
+                        .withMin(LocalDate.of(2019, 3, 1))
+                        .withMax(LocalDate.of(2019, 3, 14))
+                        .build())
+                .withXaxis(XAxisBuilder.get()
+                        .withType(XAxisType.datetime)
+                        .build());
+    }
+}
