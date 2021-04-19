7eb842a55493a734b299c3eee482b04b887059d6
Martin Vysny <mavi@vaadin.com>
2021-04-19 12:12:41 +0300

    #105 Add BoxPlot to PlotOptions

---
+++ src/main/java/com/github/appreciated/apexcharts/config/plotoptions/boxplot/Colors.java
@@ -0,0 +1,22 @@
+package com.github.appreciated.apexcharts.config.plotoptions.boxplot;
+
+public class Colors {
+    private String upper;
+    private String lower;
+
+    public String getUpper() {
+        return upper;
+    }
+
+    public void setUpper(String upper) {
+        this.upper = upper;
+    }
+
+    public String getLower() {
+        return lower;
+    }
+
+    public void setLower(String lower) {
+        this.lower = lower;
+    }
+}
