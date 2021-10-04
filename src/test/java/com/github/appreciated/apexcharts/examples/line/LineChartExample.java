038c95f7b41498fabfa2cd9f9c53852e3d6237ac
Fynn Flügge <fynnfluegge@gmx.de>
2021-10-04 14:16:09 +0200

    added TickPlacement Option to XAxis (#125)

--- src/test/java/com/github/appreciated/apexcharts/examples/line/LineChartExample.java
+++ src/test/java/com/github/appreciated/apexcharts/examples/line/LineChartExample.java
@@ -9,6 +9,7 @@ import com.github.appreciated.apexcharts.config.chart.Type;
 import com.github.appreciated.apexcharts.config.chart.builder.ZoomBuilder;
 import com.github.appreciated.apexcharts.config.grid.builder.RowBuilder;
 import com.github.appreciated.apexcharts.config.stroke.Curve;
+import com.github.appreciated.apexcharts.config.xaxis.TickPlacement;
 import com.github.appreciated.apexcharts.helper.Series;
 
 public class LineChartExample extends ApexChartsBuilder {
@@ -29,6 +30,7 @@ public class LineChartExample extends ApexChartsBuilder {
                         ).build())
                 .withXaxis(XAxisBuilder.get()
                         .withCategories("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep")
+                        .withTickPlacement(TickPlacement.between)
                         .build())
                 .withSeries(new Series<>("Desktops", 10.0, 41.0, 35.0, 51.0, 49.0, 62.0, 69.0, 91.0, 148.0));
     }
