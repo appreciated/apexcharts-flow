3ea08f0d2bc503c2fcb2e648260b7ac3d5c68709
Alexander <lanmaster@users.noreply.github.com>
2021-10-26 01:42:46 +0500

    add parameter xaxis/labels/dateTimeUTC (#128)

--- src/main/java/com/github/appreciated/apexcharts/config/xaxis/Labels.java
+++ src/main/java/com/github/appreciated/apexcharts/config/xaxis/Labels.java
@@ -18,6 +18,7 @@ public class Labels {
     private String format;
     private String formatter;
     private DatetimeFormatter datetimeFormatter;
+    private Boolean datetimeUTC;
 
     public Labels() {
     }
@@ -129,5 +130,13 @@ public class Labels {
     }
     public void setDatetimeFormatter(DatetimeFormatter datetimeFormatter) {
         this.datetimeFormatter = datetimeFormatter;
+    }
+
+    public Boolean getDatetimeUTC() {
+        return datetimeUTC;
+    }
+
+    public void setDatetimeUTC(Boolean datetimeUTC) {
+        this.datetimeUTC = datetimeUTC;
     }
 }
