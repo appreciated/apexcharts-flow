69a22646bccda3b15a28266684ab5ea4e8fa6ca9
Max <bethibande@gmail.com>
2024-07-22 14:34:06 +0200

    SeriesBuilder missing generics parameter #185 (#186)

--- src/test/java/com/github/appreciated/apexcharts/examples/line/LineChartExample.java
+++ src/test/java/com/github/appreciated/apexcharts/examples/line/LineChartExample.java
@@ -29,6 +29,9 @@ public class LineChartExample extends ApexChartsBuilder {
                         .withCategories("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep")
                         .withTickPlacement(TickPlacement.BETWEEN)
                         .build())
-                .withSeries(new Series<>("Desktops", 10.0, 41.0, 35.0, 51.0, 49.0, 62.0, 69.0, 91.0, 148.0));
+                .withSeries(SeriesBuilder.get()
+                                    .withName("Desktops")
+                                    .withData(10.0, 41.0, 35.0, 51.0, 49.0, 62.0, 69.0, 91.0, 148.0)
+                                    .build());
     }
 }
